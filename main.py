2e045741621db6d410b69db2744a1a460329d9e2
fedorovmaksimwork <fedorovmaxim.work@gmail.com>
2025-02-14 13:06:29 +0400

    Add files via upload
    init

---
+++ main.py
@@ -0,0 +1,20 @@
+# import numpy as np
+# import os
+# import cv2
+# import time
+# from ultralytics import YOLO
+#
+#
+# CONFIDENCE = 0.5
+# font_scale = 1
+# thickness = 1
+#
+# # loading the YOLOv8 model with the default weight file
+# model = YOLO("yolov8n.pt")
+#
+# # loading all the class labels (objects)
+# labels = open("data/coco.names").read().strip().split("\n")
+#
+# # generating colors for each object for later plotting
+# colors = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
+print('1231234777'[-3:])
